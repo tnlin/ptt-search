8e0b090d489fe44080e55c2418ab4c6e9a52b2f9
tnlin <sss950123@gmail.com>
2018-06-08 21:45:05 +0800

    add more example

--- nodejs-to-es/index.js
+++ nodejs-to-es/index.js
@@ -1,27 +1,37 @@
 var elasticsearch = require('elasticsearch');
 var client = new elasticsearch.Client({
     host: '13.78.14.166:9200',
-    log: 'trace'
+    // log: 'trace'
 });
 
 async function makeQuery(q) {
     try {
         q = {
           index: 'test-2018-06',
+          sort: 'date_parsed:desc',
           type: 'doc',
           body: {
             query: {
               match: {
-                article_title: '女友'
+                article_title: '西瓜 夏天'
               }
             }
           }
         }
+
         const response = await client.search(q)
+
+        console.log(response)
         for (const hit of response.hits.hits) {
-            console.log('hit:',hit);
+            console.log(
+                hit._source.message_count.count,
+                hit._source.article_title,
+                hit._source.author,
+                hit._score,
+                hit._source.date_parsed);
         }
-        // console.log(response)
+
+        // console.log(response.hits.hits)
   } catch (err) {
     console.error(err)
   }
