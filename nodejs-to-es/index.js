aee21dc49f4490eab06a5ecd2621b1f05e64aa64
tnlin <sss950123@gmail.com>
2018-06-11 23:16:25 +0800

    Finish date range filter and order, working on board filtering

--- nodejs-to-es/index.js
+++ nodejs-to-es/index.js
@@ -7,7 +7,7 @@ var client = new elasticsearch.Client({
 async function makeQuery(q) {
     try {
         const response = await client.search(q)
-        console.log(response)
+        // console.log(response)
         // console.log(response.hits)
         // console.log(response.hits.hits)
 
@@ -30,23 +30,66 @@ async function makeQuery(q) {
 
 
 const keyword = '西瓜夏天'
+
 q = {
     index: 'ptt-2018-06',
-    // sort: 'date_parsed:desc',
+    // sort: ['date_parsed:desc', '_score'],
     type: 'article',
+    size: 20,
     body: {
         query: {
-            // match: {
-            //     article_title: '西瓜夏天'
-            // }
-            multi_match: {
-                query: keyword,
-                fields: [ "article_title^3", "content" ],
-                tie_breaker: 0.3
+            bool: {
+                must: {
+                    multi_match: {
+                        query: keyword,
+                        fields: ["article_title^3", "content"],
+                        tie_breaker: 0.3
+                    }
+                },
+                filter: {
+                    // term: {
+                    //     board: "*"
+                    // },
+                    range: {
+                        date_parsed: {
+                            // gte: "now-7d/d",
+                            lt: "now/d"
+                        }
+                    }
+                }
             }
         }
     }
 }
+
+sort_by = "date"
+date_filter = "30d"
+// board_filter = "WomenTalk"
+
+if(sort_by=='date'){
+    q.sort = ['date_parsed:desc', '_score']
+} else if(sort_by=='count'){
+    q.sort = ['message_count.count:desc', '_score']
+} else if(sort_by=='controversial'){
+    q.sort = ['message_count.controversial:desc', '_score']
+}
+
+if(date_filter=='1d'){
+    q.body.query.bool.filter.range.date_parsed.gte = "now-1d/d"
+} else if(date_filter=='7d'){
+    q.body.query.bool.filter.range.date_parsed.gte = "now-7d/d"
+} else if(date_filter=='30d'){
+    q.body.query.bool.filter.range.date_parsed.gte = "now-30d/d"
+} else if(date_filter=='365d'){
+    q.body.query.bool.filter.range.date_parsed.gte = "now-365d/d"
+}
+
+// if(board_filter != ""){
+//     q.body.query.bool.filter.term = {"board": "WomenTalk"}
+// }
+
+
+console.log(q, q.body.query.bool)
 
 response = makeQuery(q)
 
